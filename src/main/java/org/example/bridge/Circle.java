b81e767d8b41bf94bf8e316cd703d3df1b301e0c
Jack-0ut <yugin12s@gmail.com>
2022-10-06 17:04:16 +0300

    Bridge Pattern Representation

---
+++ src/main/java/org/example/bridge/Circle.java
@@ -0,0 +1,16 @@
+package org.example.bridge;
+
+public class Circle extends Shape{
+    private int x,y,radius;
+    public Circle(int x,int y,int radius,DrawingAPI drawingAPI) {
+        super(drawingAPI);
+        this.x = x;
+        this.y = y;
+        this.radius = radius;
+    }
+
+    @Override
+    public void draw() {
+        drawingAPI.drawCircle(x,y,radius);
+    }
+}
